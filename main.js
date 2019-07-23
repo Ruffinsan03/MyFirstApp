ac232512e43cf4f256819971aa5842d2cba138bb
Wilisa Ruffin <51328590+Ruffinsan03@users.noreply.github.com>
2019-07-22 22:14:46 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+'use script'
+
+var Saturday = 'Winning';
+var iRelapsedMan = ('Bummer Dude! State reason for relapse');
+var rewind = ('Click Naughty little grasshopper. Rewind and eat healthy for the rest of the week, okay?');
+//alert('Stay positive and focus!');
+
+//if (Saturday == Winning) {
+    //alert('You made it! Burgers and beer for everyone');
+}
+
+
+//var Saturday = ('Winning');
+//alert('You made it! Burgers and beer for everyone!'); 
+
+console.log(iRelapsedMan);
+//<OPTION>I Relapsed Man
